22cccbcd0b8424ba8bad1449f5fa3581cb9b4023
Rui Maximo <rmaximo@lync-solutions.com>
2021-09-03 16:42:23 +0100

    renaming and more cleanup

--- scripts/secrets.py
+++
@@ -1,9 +0,0 @@
-import re
-
-filename = 'app/routes.py'
-f = open(filename, 'r')
-content = f.read()
-found = re.search('Github\(\'.+\'\)', content)
-if found:
-    print('\ngithub access token found\n')
-    exit(1)
